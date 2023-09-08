197797918ee0670274b86af3484577937ab67108
brochristensen <jamesmax@byui.edu>
2023-09-08 13:42:38 -0600

    adding nat

--- aws-terraform/mainbastionjuiceboxnat.tf
+++ aws-terraform/mainbastionjuiceboxnat.tf
@@ -89,24 +89,22 @@ resource "aws_nat_gateway" "nat_gateway" {
   }
 }
 
+#Make a route table for the NAT
 resource "aws_route_table" "routeTable_NAT" {
   vpc_id = "${aws_vpc.team_vpc.id}"
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_nat_gateway.nat_gateway.id}"
   }
-
   tags = {
     Name = "routeTable_NAT"
   }
 }
-
+#Link the route table to the private subnet
  resource "aws_route_table_association" "associateNAT" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.routeTable_NAT.id
 }
-
 
 # Create a private key, 4096-bit RSA
 resource "tls_private_key" "priv_key" {
