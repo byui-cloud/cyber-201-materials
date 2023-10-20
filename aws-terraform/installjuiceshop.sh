bd934f15315ed867568df22d090b5d8aba8bde7b
brochristensen <jamesmax@byui.edu>
2023-10-20 15:31:51 -0600

    added a name to the docker container

--- aws-terraform/installjuiceshop.sh
+++ aws-terraform/installjuiceshop.sh
@@ -4,4 +4,4 @@ sudo yum install docker
 sudo docker pull bkimminich/juice-shop
 sudo systemctl enable docker
 sudo service docker start
-sudo docker run -d --restart unless-stopped -p 80:3000 bkimminich/juice-shop
+sudo docker run --name naughty_keller -d --restart unless-stopped -p 80:3000 bkimminich/juice-shop
